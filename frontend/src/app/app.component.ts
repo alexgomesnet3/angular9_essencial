5ecf05cbc6eaba229b41b0eb48acb02e591d6f98
“Alex <alex.gomes.net3@gmail.com>
2023-05-17 12:19:57 -0300

    Created Header Component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { HeaderComponent } from "./components/template/header/header.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+  "app-header": HeaderComponent;
+}
