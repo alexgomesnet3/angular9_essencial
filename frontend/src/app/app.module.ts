f703b2dc7e7428db66ce8647d158e43e3782ed6e
“Alex <alex.gomes.net3@gmail.com>
2023-05-17 18:17:30 -0300

    Created Product Interface

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,10 +12,11 @@ import { NavComponent } from "./components/template/nav/nav.component";
 
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
