700c092cc88c4213499d5b236a070b68735390f5
“Alex <alex.gomes.net3@gmail.com>
2023-05-17 15:47:00 -0300

    Created NavComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,7 @@
 import { Component } from "@angular/core";
-import { HeaderComponent } from "./components/template/header/header.component";
-import { FooterComponent } from "./components/template/footer/footer.component";
-import { NavComponent } from "./components/template/nav/nav.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
 })
-export class AppComponent {
-  "app-header": HeaderComponent;
-  "app-footer": FooterComponent;
-  "app-nav": NavComponent;
-}
+export class AppComponent {}
